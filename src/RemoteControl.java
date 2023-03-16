c19c7b7616317232198c17056b4937c0053abb5f
강수빈 <zxc2289@naver.com>
2023-03-16 20:32:39 +0900

    리모컨 온도조절

--- src/RemoteControl.java
+++ src/RemoteControl.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /*컴퓨터실에서 수업 중인 정보 선생님은 냉난방기의 온도를 조절하려고 한다.
 냉난방기가 멀리 있어서 리모컨으로 조작하려고 하는데, 리모컨의 온도 조절 버튼은 다음과 같다.
 1) 온도를 1도 올리는 버튼
@@ -20,17 +25,38 @@
 */
 public class RemoteControl {
 	private static int solution(int a, int b) {
-		int answer = 0;
-		int[] control = {+1, -1, +5, -5, +10, -10};
-		while(true) {
-			
-			if(a==b) break;
-		}
-		return answer;
+		int answer = 0; // 반복 횟수
+	    int need = a - b; // 필요한 온도
+	    if(need < 0) need*= -1;
+	    
+	    while(true){
+	        if(need >= 10){ // 필요한 온도가 10도 이상
+	            need -= 10;
+	            answer++;
+	        }else if(need > 7){ // 필요한 온도가 8도 이상일 경우 10도를 더하고 1도를 빼는것이 최소횟수
+	            need -= 10;
+	            answer++;
+	        }else if(need >= 5){ // 필요한 온도가 5도 이상
+	            need -= 5;
+	            answer++;
+	        }else if(need > 3){ // 필요한 온도가 4도 이상일 경우 5도를 더하고 1도를 빼는것이 최소횟수
+	            need -= 5;
+	            answer++;
+	        }else if(need > 0){ // 필요한 온도가 1~3일 경우
+	            need -= 1;
+	            answer++;
+	        }else if(need == 0){ 
+	            break;
+	        }else{ // 필요한 온도가 - 인 경우
+	            need += 1;
+	            answer++;
+	        }	        
+	    }
+	    return answer; // 횟수 반환
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(solution(7, 34));
+		System.out.println(solution(0,4));
 	}
 
 }
