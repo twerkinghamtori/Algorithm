5d719a8d8a04f4caf6856b7a5b7ddb477eef69e7
강수빈 <zxc2289@naver.com>
2023-03-22 09:56:53 +0900

    소인수분해

--- src/complete/PrimeFactorization.java
+++ src/complete/PrimeFactorization.java
@@ -9,7 +9,7 @@ public class PrimeFactorization {
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
 		while(N!=1) {
-			for(int i=2; i<=Math.sqrt(N); i++) {
+			for(int i=2; i<=N; i++) {
 				if(N%i==0) {
 					System.out.println(i);
 					N/=i;
