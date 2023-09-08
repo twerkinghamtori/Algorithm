5d2f4c418352a2b76314ffce11d2b9c8cd1f56ad
twerkinghamtori <zxc2289@naver.com>
2023-09-08 18:31:26 +0900

    노트

--- src/Note.java
+++ src/Note.java
@@ -4,41 +4,39 @@ import java.io.*;
 public class Note {
 
 	public static void main(String[] args) throws IOException {
-		Scanner scan = new Scanner(System.in);
-
-		int n = scan.nextInt();
-		int five = Integer.MAX_VALUE;
-		int three = Integer.MAX_VALUE;
-		boolean b = false;
-
-		List<Integer> list = new ArrayList<>();
-
-		if (n % 5 == 0) {
-			five = n / 5;
-			list.add(five);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		
+		int[][] time = new int[n][2];
+		for(int i=0; i<time.length; i++) {
+			st = new StringTokenizer(br.readLine());
+			time[i][0] = Integer.parseInt(st.nextToken());
+			time[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		if (n % 3 == 0) {
-			three = n / 3;
-			list.add(five);
-		}
+		Arrays.sort(time, (o1,o2)-> {
+			if (o1[1] == o2[1]) {
+		        return o1[0] - o2[0];
+		    } else {
+		        return o1[1] - o2[1];
+		    }
+		});
 		
-		for(int i=0; i<=n/5; i++) {
-			for(int j=0; j<=n/3; j++) {
-				if(i*5+j*3 == n) {
-					b=true;
-					list.add(i+j);
-				}
+		int cnt = 1;
+		int end = time[0][1];
+		for(int i=1; i<n; i++) {	
+			if(time[i][0] == time[i][1]) {
+				cnt++;
+				continue;
+			} 
+			if(time[i][0] >= end) {
+				cnt++;
+				end = time[i][1];
 			}
 		}
-
-		if (five == Integer.MAX_VALUE && three == Integer.MAX_VALUE&& !b)
-			System.out.println(-1);
-		else {
-			list.sort((o1, o2) -> o1 - o2);
-			System.out.println(list.get(0));
-		}
-
+		System.out.println(cnt);
 	}
 
 }
