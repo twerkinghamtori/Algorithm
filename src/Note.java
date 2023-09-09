ab9ce28c20da2d3052ec4214f7f698661df8c8fc
twerkinghamtori <zxc2289@naver.com>
2023-09-09 21:45:32 +0900

    노트

--- src/Note.java
+++ src/Note.java
@@ -4,20 +4,22 @@ public class Note {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int m = scan.nextInt();
 		
-		int money = 1000 - m;
+		long n = scan.nextLong();
 		
-		int cnt = 0;
+		long cnt = 0;
+		long sum = 0;
+		long i = 1;
+		while(true) {
+			
+			if(sum > n) break;
+			sum += i;
+			i++;
+			cnt++;
+			
+		}
 		
-		int[] change = {500,100,50,10,5,1};
-		
-		for(int i=0; i<change.length; i++) {
-			cnt+= money/change[i];
-			money%=change[i];
-		}		
-		
-		System.out.println(cnt);
+		System.out.println(cnt-1);
 	}
 
 }
