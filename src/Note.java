877698a9fcb8c84f2e8967b6a71d09af0ab78ef6
twerkinghamtori <zxc2289@naver.com>
2023-09-09 10:28:33 +0900

    노트

--- src/Note.java
+++ src/Note.java
@@ -1,32 +1,23 @@
 import java.util.*;
-import java.io.*;
 
 public class Note {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
-		String[] minus = br.readLine().split("-");
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int m = scan.nextInt();
 		
-		int init = Integer.MAX_VALUE; //초기값 설정
+		int money = 1000 - m;
 		
-		for(int i=0; i<minus.length; i++) {
-			String[] plus = minus[i].split("\\+"); //+는 정규식에서 특수한 문자 => 이스케이프 처리 해야함
-			
-			int sum = 0;
-			for(int j=0; j<plus.length; j++) {
-				sum+=Integer.parseInt(plus[j]);
-			}
-			
-			if(init == Integer.MAX_VALUE) {
-				init = sum;
-			} else {
-				init -= sum;
-			}
-			
-		}
+		int cnt = 0;
 		
-		System.out.println(init);
+		int[] change = {500,100,50,10,5,1};
 		
+		for(int i=0; i<change.length; i++) {
+			cnt+= money/change[i];
+			money%=change[i];
+		}		
+		
+		System.out.println(cnt);
 	}
 
 }
