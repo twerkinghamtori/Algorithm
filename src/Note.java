f386ee8e69c07c0c578e58433a385e67f20519d3
twerkinghamtori <zxc2289@naver.com>
2023-09-11 18:07:30 +0900

    노트

--- src/Note.java
+++ src/Note.java
@@ -1,25 +1,39 @@
 import java.util.*;
+import java.io.*;
 
 public class Note {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		long n = scan.nextLong();
+		int t = Integer.parseInt(br.readLine());
 		
-		long cnt = 0;
-		long sum = 0;
-		long i = 1;
-		while(true) {
-			
-			if(sum > n) break;
-			sum += i;
-			i++;
-			cnt++;
-			
+		StringTokenizer st;
+		
+		int[] answer = new int[t];	
+		for(int i=0; i<t; i++) {
+			int a = Integer.parseInt(br.readLine());
+			int[][] arr = new int[a][2];
+			for(int j=0; j<a; j++) {
+				st = new StringTokenizer(br.readLine());
+				arr[j][0] = Integer.parseInt(st.nextToken());
+				arr[j][1] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(arr, (o1,o2) -> o1[0] - o2[0]); //서류 기준 정렬
+			int cnt = 1;
+			int min = arr[0][1];
+			for(int j=1; j<arr.length; j++) {				
+				if(min > arr[j][1]) {
+					cnt++;
+					min = arr[j][1];
+				} 
+			}	
+			answer[i] = cnt;
 		}
 		
-		System.out.println(cnt-1);
+		for(int i : answer) {
+			System.out.println(i);
+		}
 	}
 
 }
