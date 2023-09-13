dd1dcb154a696bff8e71587ec359a22130654865
twerkinghamtori <zxc2289@naver.com>
2023-09-13 18:45:38 +0900

    노트

--- src/Note.java
+++ src/Note.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Note {
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
