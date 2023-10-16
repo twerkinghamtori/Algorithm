864bc8ce00210af980ccaba91befdbc65da74170
twerkinghamtori <zxc2289@naver.com>
2023-10-16 20:35:23 +0900

    알고리즘

---
+++ src/search/GameMapShortestWay.java
@@ -0,0 +1,18 @@
+package search;
+
+import java.util.*;
+
+public class GameMapShortestWay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int solution(int[][] maps) {
+		int answer = 0;
+		
+		return answer;
+	} 
+
+}
