2ecbc88f9201361cf94648b17304dffdc5243633
twerkinghamtori <zxc2289@naver.com>
2023-11-21 13:25:40 +0900

    [알파벳] 백준 1987 dfs, 백트래킹 반례 수정

--- src/search/Alphabet.java
+++ src/search/Alphabet.java
@@ -12,9 +12,9 @@ public class Alphabet {
 	
 	static int[][] board;
 	static boolean[] visited = new boolean[26]; //방문한 알파벳 체크
-	static int[] dy = { -1, 1, 0, 0 };
-	static int[] dx = { 0, 0, -1, 1 };
-	static int answer = 0;
+	static int[] dy = { 0, 0, -1, 1 };
+	static int[] dx = { -1, 1, 0, 0 };
+	static int answer = 1;
 	static int h,w;
 	
 	public static void main(String[] args) throws IOException {
@@ -32,34 +32,34 @@ public class Alphabet {
 				board[i][j] = temp.charAt(j)-'A';
 			}
 		}
-		
 		dfs(0,0,0);
 		
 		System.out.println(answer);
 
 	}
 
-	private static void dfs(int y, int x, int cnt) {
-		// 이미 방문한 알파벳을 만나면 최대 길이를 업데이트하고 탐색을 종료
-		if(visited[board[y][x]]) {
-			answer = Math.max(answer , cnt);
-			return;
-		} else {
-			visited[board[y][x]] = true;
-			// 상하좌우 이동 탐색
-			for(int i=0; i<4; i++) {
-				int cy = y + dy[i];
-				int cx = x + dx[i];
-				
-				//보드 범위 내에서 이동 가능한 경우 재귀 호출
-				if(cy >= 0 && cx >=0 && cy<h && cx<w) {
-					dfs(cy, cx, cnt+1);
-				}				
-			}
-			//탐색 종료 후 방문 여부 초기화 (백트래킹)
-			visited[board[y][x]] = false;
-		}
+	private static void dfs(int x, int y, int cnt) {
+		// 이미 방문한 알파벳을 만나거나, 최대 길이에 도달하면 종료
+        if (visited[board[x][y]]) {
+            answer = Math.max(answer, cnt); // cnt가 26일 때는 모든 알파벳을 다 사용한 경우이므로 25로 업데이트
+            return;
+        } else {
+        	visited[board[x][y]] = true;
+    		
+    		// 상하좌우 이동 탐색
+    		for(int i=0; i<4; i++) {				
+    			int cx = x + dx[i];
+    			int cy = y + dy[i];
+    				
+    			//보드 범위 내에서 이동 가능한 경우 재귀 호출
+    			if(cx >= 0 && cy >=0 && cx<h && cy<w) {
+    				dfs(cx, cy, cnt+1);
+    			}				
+    		}
+    		//탐색 종료 후 방문 여부 초기화 (백트래킹)
+    		visited[board[x][y]] = false;
+        }
+		
+		
 	}
-
-
 }
