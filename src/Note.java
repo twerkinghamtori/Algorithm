a9e0c22447f8ed8729dc4dedf8464d06622a2fe5
twerkinghamtori <zxc2289@naver.com>
2023-12-08 03:27:58 +0900

    노트

--- src/Note.java
+++ src/Note.java
@@ -5,47 +5,59 @@ import java.io.*;
 public class Note {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while(true) {
-			String s = br.readLine();
-			if(s.equals("end")) break;
-			boolean can = true;
-			int cnt = 0;
-			int v = 0;
-			int w = 0;
-			for(int i=0; i<s.length(); i++) {
-				if(isVowel(s.charAt(i))) {
-					v++;
-					w=0;
-					cnt++;
-				} 
-				else {
-					w++;
-					v=0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int s = Integer.parseInt(st.nextToken());
+		int[] switches = new int[s+1];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=s; i++) {
+			switches[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		while(n>0) {
+			n--;
+			st = new StringTokenizer(br.readLine());
+			int gender = Integer.parseInt(st.nextToken());
+			int num = Integer.parseInt(st.nextToken());
+			if(gender == 1) {
+				for(int i=num; i<switches.length; i+=num) {
+					if(switches[i] == 1) switches[i] = 0;
+					else switches[i] = 1;
 				}
-//				System.out.println(v + " " + w);
-				if(w>=3 || v>=3) {
-					can = false;
-					break;
-				}
-			}
-			for(int i=0; i<s.length()-1; i++) {
-				if(s.charAt(i) == s.charAt(i+1)) {
-					if(s.charAt(i) != 'e' && s.charAt(i) != 'o') can = false;
-				}
-			}
-			if(cnt == 0) can = false;
-			if(can) {
-				System.out.println("<" + s + "> is acceptable.");
 			} else {
-				System.out.println("<" + s + "> is not acceptable.");
+				int end_point = 0;
+				for(int i=1; i<switches.length; i++) {
+					if(num-i<1 || num+i>=switches.length) break;
+					if(switches[num-i] == switches[num+i]) {
+						end_point = i;
+					} else {
+						break;
+					}
+				}
+				if(switches[num] == 1) switches[num] = 0;
+				else switches[num] = 1;
+				for(int i=1; i<=end_point; i++) {
+					if(switches[num-i] == 1) switches[num-i] = 0;
+					else switches[num-i] = 1;
+					if(switches[num+i] == 1) switches[num+i] = 0;
+					else switches[num+i] = 1;
+				}
 			}
+//			for(int i=1; i<switches.length; i++) {
+//				System.out.print(switches[i] + " ");
+//			}
+//			System.out.println();
+		}
+		
+		for (int i = 1; i < switches.length; i++) {
+		    System.out.print(switches[i] + " ");
+		    if (i % 20 == 0) {
+		        System.out.println();
+		    }
 		}
 		
 	}
-	
-	private static boolean isVowel(char c) {
-		if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
-		else return false;
-	}
-
 }
