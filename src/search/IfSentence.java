9fc52a9b9189bf05d144a1c497fbecf5a6a6cd52
twerkinghamtori <zxc2289@naver.com>
2023-12-12 08:12:26 +0900

    [IF문 좀 대신 써줘] 백준 19637 이분탐색

---
+++ src/search/IfSentence.java
@@ -0,0 +1,48 @@
+package search;
+
+
+import java.util.*;
+import java.io.*;
+
+//백준 19637 IF문 좀 대신 써줘 이분탐색
+//시간초과 -> 이분탐색으로 해결
+public class IfSentence {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		String[][] title=new String[n][2];
+		
+		for(int i=0;i<n;i++){
+		    st=new StringTokenizer(br.readLine());
+		    title[i][0]=st.nextToken();
+		    title[i][1]=st.nextToken();
+		}
+		
+		//이분탐색
+		for(int i=0;i<m;i++){
+            int num=Integer.parseInt(br.readLine());
+
+            int left=0;
+            int right=n-1;
+            int mid=(left+right)/2;
+            while(left<=right){
+                mid=(left+right)/2;
+
+                int titleInt=Integer.parseInt(title[mid][1]);
+                if(titleInt<num){
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
+            }
+
+            bw.write(title[left][0]+"\n");
+        }
+		bw.flush();
+	}
+}
