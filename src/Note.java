d0ab4408c7f9fe1901ecfcaf0c6e056e2b662379
twerkinghamtori <zxc2289@naver.com>
2023-12-28 14:46:43 +0900

    노트

--- src/Note.java
+++ src/Note.java
@@ -4,60 +4,72 @@ import java.io.*;
 
 public class Note {
 	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		int[][] board = new int[n][2];
-		
-		int max = 0;
-		for(int i=0; i<n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			board[i][0] = Integer.parseInt(st.nextToken());
-			board[i][1] = Integer.parseInt(st.nextToken());
-			
-			max = Math.max(board[i][1], max);
-		}
-		
-		Arrays.sort(board, (o1,o2) -> o1[0] - o2[0]);
-		
-		//가장 큰 기둥 중 가장 왼쪽에 있는 기둥
-		int start = 0;
-		for(int i=0; i<n; i++) {
-			if(board[i][1] == max) {
-				start = i;
-			}
-		}
-		
-		//가장 높은 기둥은 직사각형 1개로 치고, 높이 처음에 더해줌
-		int answer = max; 
-		
-		int curL = board[0][0];
-		int curH = board[0][1];
-		
-		//처음부터 가장 큰 기둥까지 탐색 (왼쪽)
-		for(int i=0; i<=start; i++) {
-			//이전 기둥보다 큰 기둥이 나타나면 사각형 넓이 구하기
-			if(board[i][1] >= curH) {
-				answer += (board[i][0]-curL)*curH;
-				//현재 기둥 갱신
-				curL = board[i][0];
-				curH = board[i][1];
-			}
-		}
-		
-		curL = board[n-1][0];
-		curH = board[n-1][1];
-		
-		//오른쪽부터 가장 큰 기둥까지 역으로 탐색 (오른쪽)
-		for(int i=n-1; i>=start; i--) {
-			if(board[i][1] >= curH) {
-				answer += (curL - board[i][0])*curH;
-				curL = board[i][0];
-				curH = board[i][1];
-			}
-		}
-		
-		System.out.println(answer);
+		String m = "A";
+		String[] musicinfos = {"12:00,12:01,Sing,A", "12:00,12:01,Song,A"};
+		System.out.println(solution(m,musicinfos));
 	}
+	
+	public static String solution(String m, String[] musicinfos) {
+String answer = "";
+        
+        int lastruntime = 0;
+        
+        for(String s : musicinfos) {
+            String[] sarr = s.split(",");
+            int startH = Integer.parseInt(sarr[0].substring(0,2));
+            int startM = Integer.parseInt(sarr[0].substring(3,5));
+            int endH = Integer.parseInt(sarr[1].substring(0,2));
+            int endM = Integer.parseInt(sarr[1].substring(3,5));
+            int runtime = (endH-startH)*60 + (endM - startM);
+            
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<sarr[3].length()-1; i++) {
+                String temp = sarr[3].substring(i,i+2);
+                if(temp.charAt(1) == '#') {
+                    sb.append(temp.substring(0,1).toLowerCase());
+                } else if(temp.charAt(0) == '#') {
+                    continue;
+                } else {
+                    sb.append(temp.substring(0,1));
+                }
+            }
+            if(sarr[3].charAt(sarr[3].length()-1) != '#') {
+                sb.append(sarr[3].substring(sarr[3].length()-1,sarr[3].length()));
+            }
+            //System.out.println(sb.toString());
+            
+            String melody = "";
+            int idx = 0;
+            while(idx < runtime) {
+                String code = sb.substring(idx%sb.length(),idx%sb.length()+1); 
+                if(code.equals(code.toLowerCase())) {
+                    melody += code.toUpperCase() + "#";
+                } else {
+                    melody += code;
+                }
+                idx++;
+            }
+            //System.out.println(melody);
+            int c = melody.indexOf(m);
+            
+            System.out.println(c);
+            
+            if(c != -1) {
+            	if(c+m.length() >= melody.length()) {
+            		if(runtime > lastruntime ) {
+            			answer = sarr[2];
+                		lastruntime = runtime;
+            		}
+            	} else if(!melody.substring(c+m.length(),c+m.length()+1).equals("#")) {
+            		if(runtime > lastruntime ) {
+            			answer = sarr[2];
+                		lastruntime = runtime;
+            		}
+            	} 
+            }
+            System.out.println(runtime + " " + lastruntime);
+        }
+        return answer.equals("")?"(None)":answer;
+    }
 }
 
