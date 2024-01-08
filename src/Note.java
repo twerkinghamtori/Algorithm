813cf8c561290992e3cafac89c93d289e20ffca3
twerkinghamtori <zxc2289@naver.com>
2024-01-08 18:54:42 +0900

    연습노트

--- src/Note.java
+++ src/Note.java
@@ -2,74 +2,87 @@
 import java.util.*;
 import java.io.*;
 
-public class Note {
-	public static void main(String args[]) throws IOException {
-		String m = "A";
-		String[] musicinfos = {"12:00,12:01,Sing,A", "12:00,12:01,Song,A"};
-		System.out.println(solution(m,musicinfos));
+class Path implements Comparable<Path> {
+	int end;
+	int weight;
+	
+	Path(int end, int weight) {
+		this.end = end;
+		this.weight = weight;
 	}
 	
-	public static String solution(String m, String[] musicinfos) {
-String answer = "";
-        
-        int lastruntime = 0;
-        
-        for(String s : musicinfos) {
-            String[] sarr = s.split(",");
-            int startH = Integer.parseInt(sarr[0].substring(0,2));
-            int startM = Integer.parseInt(sarr[0].substring(3,5));
-            int endH = Integer.parseInt(sarr[1].substring(0,2));
-            int endM = Integer.parseInt(sarr[1].substring(3,5));
-            int runtime = (endH-startH)*60 + (endM - startM);
-            
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i<sarr[3].length()-1; i++) {
-                String temp = sarr[3].substring(i,i+2);
-                if(temp.charAt(1) == '#') {
-                    sb.append(temp.substring(0,1).toLowerCase());
-                } else if(temp.charAt(0) == '#') {
-                    continue;
-                } else {
-                    sb.append(temp.substring(0,1));
-                }
-            }
-            if(sarr[3].charAt(sarr[3].length()-1) != '#') {
-                sb.append(sarr[3].substring(sarr[3].length()-1,sarr[3].length()));
-            }
-            //System.out.println(sb.toString());
-            
-            String melody = "";
-            int idx = 0;
-            while(idx < runtime) {
-                String code = sb.substring(idx%sb.length(),idx%sb.length()+1); 
-                if(code.equals(code.toLowerCase())) {
-                    melody += code.toUpperCase() + "#";
-                } else {
-                    melody += code;
-                }
-                idx++;
-            }
-            //System.out.println(melody);
-            int c = melody.indexOf(m);
-            
-            System.out.println(c);
-            
-            if(c != -1) {
-            	if(c+m.length() >= melody.length()) {
-            		if(runtime > lastruntime ) {
-            			answer = sarr[2];
-                		lastruntime = runtime;
-            		}
-            	} else if(!melody.substring(c+m.length(),c+m.length()+1).equals("#")) {
-            		if(runtime > lastruntime ) {
-            			answer = sarr[2];
-                		lastruntime = runtime;
-            		}
-            	} 
-            }
-            System.out.println(runtime + " " + lastruntime);
-        }
-        return answer.equals("")?"(None)":answer;
-    }
+	@Override
+	public int compareTo(Path p) {
+		return weight - p.weight;
+	}
+}
+public class Note {
+	static final int INF = 987654321;
+	static ArrayList<ArrayList<Path>> list;
+	static int V,E,K;
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		
+		K = Integer.parseInt(br.readLine());
+		
+		list = new ArrayList<>();
+		for(int i=0; i<=V; i++) {
+			list.add(new ArrayList<>());
+		}
+		
+		for(int i=0; i<E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			list.get(start).add(new Path(end, weight));
+		}
+		
+		int[] dist = dijkstra(list);
+		
+		for(int i=1; i<=V; i++) {
+			if(dist[i] == INF) {
+				bw.write("INF" + "\n");
+			} else {
+				bw.write(dist[i] + "\n");
+			}
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+	static int[] dijkstra(ArrayList<ArrayList<Path>> a) {
+		PriorityQueue<Path> pq = new PriorityQueue<>();
+		pq.add(new Path(K,0));
+		
+		boolean[] check = new boolean[V+1];
+		int[] dist = new int[V+1];
+		Arrays.fill(dist, INF);
+		dist[K] = 0;
+		
+		while(!pq.isEmpty()) {
+			Path curPath = pq.poll();
+			int cur = curPath.end;
+			
+			if(!check[cur]) {
+				check[cur] = true;
+				
+				for(Path p : a.get(cur)) {
+					if(!check[p.end] && dist[p.end] > dist[cur] + p.weight) {
+						dist[p.end] = dist[cur] + p.weight;
+						pq.add(new Path(p.end, dist[p.end]));
+					}
+				}
+			}
+		}
+		return dist;
+	}
 }
 
