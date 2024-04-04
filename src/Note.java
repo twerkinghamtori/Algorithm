6f0554dd1ebad20f63508c051ad02d487f4f5a4d
twerkinghamtori <zxc2289@naver.com>
2024-04-04 14:09:09 +0900

    노트

--- src/Note.java
+++ src/Note.java
@@ -3,79 +3,131 @@ import java.util.*;
 import java.io.*;
 
 public class Note {
-	static boolean[] visited;
-	static int n;
-	static StringBuilder sb = new StringBuilder();
-	static int[][] board;
-	static Queue<Integer> q = new LinkedList<>();
+
+	static int[] X = {1,-1,0,0};
+	static int[] Y = {0,0,1,-1};
+	static int n,m;
+	static int[][] arr;
 	
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int v = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 		
-		board = new int[n+1][n+1];
+		arr = new int[n][m];
 		
-		while(m>0) {
-			m--;
+		
+		Queue<int[]> q = new LinkedList<>(); //빙하가 있는 위치를 담을 큐
+		
+		for(int i=0; i<n; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			board[a][b] = 1;
-			board[b][a] = 1;
-		}
-		visited = new boolean[n+1];
-		
-		dfs(v, 0);
-		
-		sb.append("\n");
-		visited = new boolean[n+1];
-		bfs(v,0);
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-
-	private static void dfs(int v, int depth) {
-		if(depth == n) {
-			return;
-		}
-		
-		if(!visited[v]) {
-			visited[v] = true;
-			sb.append(v + " ");
-			for(int i=0; i<n+1; i++) {
-				if(board[v][i] == 1 && !visited[i]) {
-					dfs(i, depth+1);
-				}
+			for(int j=0; j<m; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				if(arr[i][j] != 0) {
+					q.add(new int[] {i,j});
+				}				
 			}
 		}
-	}
-
-	private static void bfs(int v, int depth) {
-		if(!visited[v]) {
-			q.add(v);
-			visited[v] = true;
+		//입력 끝
+		
+		int cnt = 0;
+		
+		while(!q.isEmpty()) {
 			
-			while(!q.isEmpty()) {
-				v = q.poll();
-				sb.append(v + " ");
+			//덩어리 개수가 2개 이상이면 exit
+			if(calSection(arr) >= 2) {
+				break;
+			}
+			
+			int size = q.size();
+			
+			List<int[]> zerocnt = new ArrayList<>(); //인접한 면의 0의 개수를 저장할 리스트
+			
+			for(int i=0; i<size; i++) {
+				int[] cur = q.poll();
+				int x = cur[0];
+				int y = cur[1];
+				int zero = 0;
 				
-				for(int i=1; i<n+1; i++) {
-					if(board[v][i] == 1 && !visited[i]) {
-						q.add(i);
-						visited[i]=true;
+				for(int j=0; j<4; j++) {
+					int newx = x + X[j];
+					int newy = y + Y[j];
+					
+					if(newx<0 || newy<0 || newx>=n || newy>=m) continue;
+					
+					if(arr[newx][newy] == 0) {
+						zero++;
+					}
+				}
+				zerocnt.add(new int[] {x,y,zero});
+			}			
+			
+			//0의 개수만큼 arr 배열 수정(빙하 높이)
+			for(int[] a : zerocnt) {
+				if(arr[a[0]][a[1]] - a[2] < 0) {
+					arr[a[0]][a[1]] = 0;
+				} else {
+					arr[a[0]][a[1]] -= a[2];
+				}
+			}
+			
+			//아직 녹지 않은 빙하의 위치 큐 삽입
+			for(int i=0; i<n; i++) {
+				for(int j=0; j<m; j++) {
+					if(arr[i][j] > 0) {
+						q.add(new int[] {i,j});
 					}
 				}
 			}
+			
+			//빙하가 다 녹았을 때,
+			if(q.isEmpty()) {
+				//덩어리가 2개 이상으로 나누어지지 않는다면 0 출력
+				if(calSection(arr) < 2) {
+					cnt = 0;
+				}
+				break;
+			}
+			cnt++;
+			
 		}
 		
+		System.out.println(cnt);
+	}
+	
+	//덩어리 계산
+	private static int calSection(int[][] arr) {
+		int cnt = 0;
+		boolean[][] visited = new boolean[n][m];
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				if(arr[i][j] > 0 && !visited[i][j]) {
+					dfs(i,j, visited);
+					cnt++;
+				}
+			}
+		}
+		return cnt;
+	}
+	
+	private static void dfs(int x, int y, boolean[][] visited) {
+		
+		if(!visited[x][y]) {
+			visited[x][y] = true;
+			
+			for(int i=0; i<4; i++) {
+				int newx = x + X[i];
+				int newy = y + Y[i];
+				
+				if(newx<0 || newy<0 || newx>=n || newy>=m) continue;
+				
+				if(arr[newx][newy] > 0) {
+					dfs(newx, newy, visited);
+				}
+			}
+		}
 	}
 	
 }
