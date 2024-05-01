e28522105ed92c7ca277325cc0078b0adeeb3828
twerkinghamtori <zxc2289@naver.com>
2024-05-01 19:13:55 +0900

    노트

--- src/Note1.java
+++ src/Note1.java
@@ -2,83 +2,85 @@ import java.io.*;
 import java.util.*;
 
 public class Note1 {
+	
+	static int[] X = {0,0,1,-1};
+	static int[] Y = {1,-1,0,0};
+	
+	static int max = Integer.MIN_VALUE;
+	static int n,m;
+	
+	static boolean[][] visited;
+	static int[][] board;
+	
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int t = Integer.parseInt(br.readLine());
-
-		ArrayList<int[]> a;
-		ArrayList<ArrayList<Integer>> graph;
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		board = new int[n][m];
 		
-		StringBuilder sb = new StringBuilder();
-		
-		while(t-- > 0) {
-
-			int storeNum = Integer.parseInt(br.readLine());
-			
-			a = new ArrayList<>();
-			
-			for(int i=0; i<storeNum+2; i++) {
-				st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				
-				a.add(new int[] {x,y});
-			}
-			
-			graph = new ArrayList<>();
-			
-			for(int i=0; i<storeNum+2; i++) {
-				graph.add(new ArrayList<>());
-			}
-			
-			//맨해튼 거리 1000m이하를 만족하는 두 정점 찾음
-			//graph에 양방향 그래프로 이어줌
-			for(int i=0; i<storeNum+2; i++) {
-				for(int j=i+1; j<storeNum+2; j++) {
-					if(Manhattan(a.get(i),a.get(j)) <= 1000) {
-						graph.get(i).add(j);
-						graph.get(j).add(i);
-					}
-				}
-			}
-			
-			sb.append((bfs(graph, storeNum) ? "happy" : "sad") + "\n");
-		}	
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
-	} 
-	
-	private static int Manhattan(int[] a, int[] b) {
-		return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
-	}
-	
-	private static boolean bfs(ArrayList<ArrayList<Integer>> graph, int N) {
-		Queue<Integer> q = new LinkedList<>();
-		q.offer(0);
-		
-		boolean[] visited = new boolean[N+2];
-		visited[0] = true;
-		
-		while(!q.isEmpty()) {
-			int now = q.poll();
-			
-			if(now == N+1) {
-				return true;
-			}
-			
-			for(int next : graph.get(now)) {
-				if(!visited[next]) {
-					visited[next] = true;
-					q.offer(next);
-				}
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<m; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		return false;
+		//입력 끝
+		
+		visited = new boolean[n][m];
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				visited[i][j] = true;
+				dfs(i, j, 1, board[i][j]);
+				visited[i][j] = false;
+				
+				combi(0, 0, i, j, board[i][j]); //인접한 4칸 중 3칸 고르기(ㅗ,ㅓ,ㅏ,ㅜ);
+			}
+		}
+		
+		System.out.println(max);
+		
+		br.close();
+		
+	} 
+	
+	private static void dfs(int x, int y, int depth, int sum) {
+		if(depth == 4) {
+			max = Math.max(max, sum);
+			return;
+		}
+		
+		for(int i=-0; i<4; i++) {
+			int newx = x + X[i];
+			int newy = y + Y[i];
+			
+			if(newx<0 || newy<0 || newx>=n || newy>=m) continue;
+			if(visited[newx][newy]) continue;
+			
+			visited[newx][newy] = true;
+			dfs(newx, newy, depth+1, sum+board[newx][newy]);
+			visited[newx][newy] = false;
+		}
 	}
+	
+	private static void combi(int depth, int start, int x, int y, int sum) {
+		if(depth == 3) {
+			max = Math.max(max, sum);
+			return;
+		}
+		
+		for(int d=start; d<4; d++) {
+			int newx = x + X[d];
+			int newy = y + Y[d];
+			
+			if(newx<0 || newy<0 || newx>=n || newy>=m) continue;
+			
+			combi(depth+1, d+1, x, y, sum+board[newx][newy]);	
+		}
+	}
+	
+	
 }
